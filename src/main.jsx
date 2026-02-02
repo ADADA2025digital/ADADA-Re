a5d4005089e9525d31e9521924459226981027de
Deniya-Edwinraj <deniyaedwinraj@gmail.com>
2026-02-02 23:21:16 +0530

    v1

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import "./assets/styles/style.css";
+import "./assets/Styles/Style.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
